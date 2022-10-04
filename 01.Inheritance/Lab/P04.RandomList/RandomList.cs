6799ca94345a1e3a12f333a7a867f88f186b2843
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-10-04 10:23:23 +0300

    Added P04

---
+++ 01.Inheritance/Lab/P04.RandomList/RandomList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random rnd = new Random();
+
+        public RandomList()
+        {
+            this.rnd = new Random();
+        }
+
+        public string RandomString()
+        {
+            return this[this.rnd.Next(0, this.Count)];
+        }
+    }
+}
---
+++ 01.Inheritance/Lab/P04.RandomList/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
