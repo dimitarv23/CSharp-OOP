2bbfa947acad2d41a1f21ca8274e72f16e4d4906
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-10-04 10:23:29 +0300

    Added P05

---
+++ 01.Inheritance/Lab/P05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return Count == 0;
+        }
+
+        public void AddRange(Stack<string> range)
+        {
+            foreach (var item in range)
+            {
+                this.Push(item);
+            }
+        }
+    }
+}
