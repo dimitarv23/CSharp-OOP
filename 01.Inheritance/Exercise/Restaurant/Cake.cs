1c2dbc9418480873ac9cb4aede2c2f264cd98c01
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-10-05 21:19:16 +0300

    Added Restraunt

---
+++ 01.Inheritance/Exercise/Restaurant/Cake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        public Cake(string name) : base(name, 0, 0, 0)
+        {
+        }
+
+        public override double Grams => 250;
+        public override double Calories => 1000;
+        public override decimal Price => 5;
+    }
+}
---
+++ 01.Inheritance/Exercise/Restaurant/Coffee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        public Coffee(string name, double caffeine) : base(name, 0, 0)
+        {
+            this.Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; set; }
+        public override double Milliliters => 50;
+        public override decimal Price => 3.50m;
+    }
+}
