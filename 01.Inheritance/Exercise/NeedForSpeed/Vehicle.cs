a7145bb621fe24e2dfa734449366b71a162b6e55
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-10-05 21:18:47 +0300

    Added NeedForSpeed

---
+++ 01.Inheritance/Exercise/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        public Vehicle(int horsePower, double fuel)
+        {
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
+        }
+
+        public double DefaultFuelConsumption = 1.25;
+        public virtual double FuelConsumption { get { return this.DefaultFuelConsumption; } }
+        public double Fuel { get; set; }
+        public int HorsePower { get; set; }
+
+        public virtual void Drive(double km)
+        {
+            this.Fuel -= km * this.FuelConsumption;
+        }
+    }
+}
