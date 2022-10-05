f81957bb461b3429d71e2c1133c4bcfbec3704cd
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-10-05 21:18:54 +0300

    Added Person

---
+++ 01.Inheritance/Exercise/Person/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Age cannot be negative!");
+                }
+
+                age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.Append($"Name: {this.Name}, Age: {this.Age}");
+
+            return sb.ToString();
+        }
+    }
+}
