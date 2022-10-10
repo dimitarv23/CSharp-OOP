20219552f01800bce783c0550074aa28ca0d2578
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-10-10 11:08:17 +0300

    Added P01

---
+++ 02.Encapsulation/Exercise/P01.ClassBoxData/Box.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace P01.ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get { return length; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+
+                length = value;
+            }
+        }
+        public double Width
+        {
+            get { return width; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+
+                width = value;
+            }
+        }
+        public double Height
+        {
+            get { return height; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+
+                height = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            double surfaceArea = 2 * (this.Length * this.Width + this.Length * this.Height + this.Width * this.Height);
+            return surfaceArea;
+        }
+        public double LateralSurfaceArea()
+        {
+            double lateralSurfaceArea = 2 * this.Height * (this.Length + this.Width);
+            return lateralSurfaceArea;
+        }
+        public double Volume()
+        {
+            double volume = this.Length * this.Width * this.Height;
+            return volume;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"Surface Area - {this.SurfaceArea():f2}");
+            sb.AppendLine($"Lateral Surface Area - {this.LateralSurfaceArea():f2}");
+            sb.AppendLine($"Volume - {this.Volume():f2}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ 02.Encapsulation/Exercise/P01.ClassBoxData/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace P01.ClassBoxData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double length = double.Parse(Console.ReadLine());
+                double width = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+
+                var box = new Box(length, width, height);
+
+                Console.WriteLine(box);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
