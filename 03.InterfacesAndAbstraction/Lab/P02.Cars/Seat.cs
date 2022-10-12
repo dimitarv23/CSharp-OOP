9797241860964d774775e9b3781a8eaf7caadbfa
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-10-12 11:42:19 +0300

    Added P02

---
+++ 03.InterfacesAndAbstraction/Lab/P02.Cars/Seat.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        private string model;
+        private string color;
+
+        public Seat(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Model cannot be empty.");
+                }
+
+                model = value;
+            }
+        }
+        public string Color
+        {
+            get { return color; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Color cannot be empty.");
+                }
+
+                color = value;
+            }
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Color} Seat {this.Model}";
+        }
+    }
+}
---
+++ 03.InterfacesAndAbstraction/Lab/P02.Cars/Tesla.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        private string model;
+        private string color;
+        private int battery;
+
+        public Tesla(string model, string color, int battery)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Battery = battery;
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Model cannot be empty.");
+                }
+
+                model = value;
+            }
+        }
+        public string Color
+        {
+            get { return color; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Color cannot be empty.");
+                }
+
+                color = value;
+            }
+        }
+        public int Battery
+        {
+            get { return battery; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Batteries cannot be 0 or negative.");
+                }
+
+                battery = value;
+            }
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Color} Tesla {this.Model} with {this.Battery} Batteries";
+        }
+    }
+}
