8c5383293f45104f94b30104f27aee53a8553d2d
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-10-24 21:18:40 +0300

    Added P03

---
+++ 04.Polymorphism/P03.Shapes/Circle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public double Radius
+        {
+            get { return radius; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Radius cannot be 0 or negative.");
+                }
+
+                radius = value;
+            }
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * this.Radius * this.Radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * this.Radius;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
---
+++ 04.Polymorphism/P03.Shapes/Rectangle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public Rectangle(double height, double width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public double Height
+        {
+            get => height;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be 0 or negative.");
+                }
+
+                height = value;
+            }
+        }
+        public double Width
+        {
+            get => width;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be 0 or negative.");
+                }
+
+                width = value;
+            }
+        }
+
+        public override double CalculateArea()
+        {
+            return this.Height * this.Width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * (this.Height + this.Width);
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
