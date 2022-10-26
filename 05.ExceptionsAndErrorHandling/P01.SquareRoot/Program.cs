be180fc9c60a014b055f760342db6d6f5cf29b32
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-10-26 23:09:19 +0300

    Added P01

---
+++ 05.ExceptionsAndErrorHandling/P01.SquareRoot/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace P01.SquareRoot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                var n = uint.Parse(Console.ReadLine());
+
+                var result = Math.Sqrt(n);
+
+                Console.WriteLine(result);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid number.");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
