fbc886d7dd509edbe7e41fbddc3f1e72d56a3e68
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-11-07 11:25:45 +0200

    Added P06

---
+++ 07.ReflectionAndAttributes/Lab/P06.CodeTracker/Tracker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+
+            foreach (var method in type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic
+                | BindingFlags.Static | BindingFlags.Instance))
+            {
+                var attrs = method.GetCustomAttributes(false);
+
+                foreach (var attr in attrs)
+                {
+                    AuthorAttribute authorAttr = attr as AuthorAttribute;
+
+                    if (authorAttr != null)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {authorAttr.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
