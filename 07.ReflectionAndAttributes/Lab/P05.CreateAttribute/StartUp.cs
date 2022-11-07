e9a235dd59cdbf1a3f7bebe70e54da10c7afba29
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-11-07 11:25:36 +0200

    Added P05

---
+++ 07.ReflectionAndAttributes/Lab/P05.CreateAttribute/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Viktor")]
+    public class StartUp
+    {
+        [Author("George")]
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
