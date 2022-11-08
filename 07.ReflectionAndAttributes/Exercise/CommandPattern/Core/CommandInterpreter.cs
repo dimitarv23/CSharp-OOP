1a09f33c59e2a141f27c947309f358722733fd26
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-11-08 12:29:27 +0200

    Added CommandPattern

---
+++ 07.ReflectionAndAttributes/Exercise/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,39 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] commandArgs = args.Split();
+
+            string cmdType = commandArgs[0];
+            string[] arguments = commandArgs.Skip(1).ToArray();
+
+            Type type = Assembly.GetCallingAssembly()
+                .GetTypes().FirstOrDefault(x => x.Name == cmdType + "Command");
+
+            if (type == null)
+            {
+                throw new InvalidOperationException("Missing command");
+            }
+
+            Type commandInterface = type.GetInterface("ICommand");
+            if (commandInterface == null)
+            {
+                throw new InvalidOperationException("Not a command");
+            }
+
+            var command = Activator.CreateInstance(type) as ICommand;
+
+            string result = command.Execute(arguments);
+            return result;
+        }
+    }
+}
---
+++ 07.ReflectionAndAttributes/Exercise/CommandPattern/Core/Engine.cs
@@ -0,0 +1,36 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace CommandPattern.Core
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                try
+                {
+                    string input = Console.ReadLine();
+                    string output = commandInterpreter.Read(input);
+
+                    Console.WriteLine(output);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
