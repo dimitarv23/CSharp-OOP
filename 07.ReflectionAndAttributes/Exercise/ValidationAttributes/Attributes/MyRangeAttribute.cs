e65dd4013a24417d11e48f4242789c52f674f585
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-11-09 09:32:34 +0200

    Added ValidationAttributes

---
+++ 07.ReflectionAndAttributes/Exercise/ValidationAttributes/Attributes/MyRangeAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationAttributes.Attributes
+{
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        private readonly int minValue;
+        private readonly int maxValue;
+
+        public MyRangeAttribute(int minValue, int maxValue)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+
+        public override bool IsValid(object obj)
+        {
+            int currValue = (int)obj;
+
+            return currValue >= minValue && currValue <= maxValue;
+        }
+    }
+}
---
+++ 07.ReflectionAndAttributes/Exercise/ValidationAttributes/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ValidationAttributes
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var person = new Person
+             (
+                 "StoSho",
+                 25
+             );
+
+            bool isValidEntity = Validator.IsValid(person);
+
+            Console.WriteLine(isValidEntity);
+        }
+    }
+}
